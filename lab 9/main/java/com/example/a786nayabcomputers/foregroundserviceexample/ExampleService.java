d527b9e3bbecb69b07b23e1dfd6f9fb950a8dfbe
hira890 <44391989+hira890@users.noreply.github.com>
2019-01-25 00:16:53 +0500

    Add files via upload

---
+++ lab 9/main/java/com/example/a786nayabcomputers/foregroundserviceexample/ExampleService.java
@@ -0,0 +1,60 @@
+package com.example.a786nayabcomputers.foregroundserviceexample;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import static com.example.a786nayabcomputers.foregroundserviceexample.App.CHANNEL_ID;
+
+
+public class ExampleService   extends Service {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+       String input =intent.getStringExtra("inputExtra");
+
+       Intent notificationIntent= new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity( this,
+               0,notificationIntent,0);
+
+        Notification notification =new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setContentTitle("ExampleService")
+                .setContentText(input)
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1,notification);
+        stopSelf();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
+
+
+
+
+
+
+
